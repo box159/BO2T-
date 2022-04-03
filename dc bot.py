946598bf34b299f49f493c7c5d3c5a6b979115dd
box159 <aa94022728@gmail.com>
2022-04-03 16:02:09 +0800

    update joke png command optimize code

--- dc bot.py
+++ dc bot.py
@@ -1,13 +1,32 @@
+import datetime
 from http import client
 import json
-from lib2to3.pgen2 import token
-from unittest import case
-from webbrowser import get
 import requests
 import discord
-client = discord.Client()
-with open('config.json') as f:  
-    token = json.load(f)["token"]
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='b!')
+
+with open('config.json','r') as f:  
+    odata = json.load(f)
+
+def get_jokepng():
+    resopnse = requests.get("https://memes.tw/wtf/api")
+    data = json.loads(resopnse.text)
+    num = odata["image"]["image_count"]
+    day = datetime.datetime.now().day
+    if(day != odata["image"]["day"]):
+        odata["image"]["day"] = day
+        num = 1
+
+    if(num == len(data)):
+        num = 1
+
+    odata["image"]["image_count"]=num+1
+    with open("config.json",'w') as f:
+        json.dump(odata,f,indent=4) 
+
+    return data[num]["src"]
 
 def get_quote():
     response= requests.get("https://v1.hitokoto.cn/")
@@ -18,36 +37,45 @@ def get_quote():
 @client.event
 async def on_ready():
     print("哈囉~我是",client.user)
-    game = discord.Game("made by box159")
+    game = discord.Game("made by box159 | b!")
     await client.change_presence(status=discord.Status.online,activity=game)
-
 
 @client.event
 async def on_message(message):
+    await client.process_commands(message)
     if message.author == client.user:
         return
-    if message.content.startswith("b2"):
-        temp = message.content.split("b2",2)
-        command = temp[1]
-        if command == "?":
-            await message.channel.send("""           
+    if message.content=="b!":
+        await message.channel.send("""           
 --------------------
-b2? - 開啟指令介面
-b2inspire - 發送心靈雞湯
+b! - 開啟指令介面
+b!inspire - 發送心靈雞湯
+b!joke - 發送梗圖
 --------------------
 """)
-        if command=="inspire":
-            await message.channel.send(get_quote())
-    if message.content == 'ping':
+    elif message.content == 'ping':
         await message.channel.send('pong')
-    if message.content.startswith("說"):
+    elif message.content.startswith("說"):
         temp = message.content.split(" ",2)
         if(len(temp)==1):
             await message.channel.send("你要我說什麼啦？")
         else :
             await message.channel.send(temp[1])
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(round(client.latency*1000),'(ms)')
+
+@client.command()    
+async def joke(ctx):
+    await ctx.send(get_jokepng())
+
+@client.command() 
+async def inspire(ctx):
+    await ctx.send(get_quote())
+
 @client.event
 async def on_disconnect():
     await client.change_presence(status=discord.Status.offline)
-client.run(token)
+
+client.run(odata["token"])
